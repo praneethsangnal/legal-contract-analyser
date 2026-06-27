eedeef76f7e73b37bf084111150257cf8013777f
Praneeth Sangnal <praneethsangnal8305@gmail.com>
2026-06-28 04:19:08 +0530

    preparing for frontend deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://legal-contract-analyser.vercel.app"
+    "https://legal-contract-analyser-one.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
